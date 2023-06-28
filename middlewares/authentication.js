b1b42f334d56996c256d04b64c1f8b9dae71b726
AlexRose2603 <alexandra26korchak@gmail.com>
2023-06-28 18:31:02 +0300

    checking token

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -7,13 +7,13 @@ const { SECRET_KEY } = process.env;
 const authentication = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+  if (bearer !== "Bearer" || !token) {
     next(HttpError(401));
   }
+
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-    console.log(user);
     if (!user || !user.token || user.token !== token) {
       next(HttpError(401));
     }
